51cd9f2db37bdcc2401d41d1e808db8c3d278d49
Roberto Arias <walom.090@gmail.com>
2015-10-08 00:35:57 -0400

    Formalized tracker GET requests and added client ID randomizer

--- src/RUBTClient.java
+++ src/RUBTClient.java
@@ -4,6 +4,7 @@ import model.TorrentStats;
 import utils.BencodingException;
 import utils.TorrentInfo;
 import utils.Utils;
+import utils.Bencoder2;
 
 import java.io.File;
 import java.io.IOException;
@@ -11,6 +12,10 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.HashMap;
+import java.util.Map;
+import java.nio.ByteBuffer;
+import java.util.List;
+import java.util.Random;
 
 import static utils.Utils.printlnLog;
 
@@ -18,8 +23,9 @@ public class RUBTClient {
 
 
 	// The peer id of this client
-	private static final String PEER_ID_PREFIX = "nc409RUgmsohbx_";
-	private static final String PEER_ID = PEER_ID_PREFIX + System.currentTimeMillis();
+	//private static final String PEER_ID_PREFIX = "nc409RUgmsohbx_";
+	//private static final String PEER_ID = PEER_ID_PREFIX + System.currentTimeMillis();
+	private static String peer_id;
 
 	// The ports this client might use (range from lowest to highest)
 	private static final String LOWEST_PORT_TO_USE = "6881";
@@ -77,6 +83,10 @@ public class RUBTClient {
 
 		printlnLog("----------- Operations with tracker ----------");
 
+		// Generate peerID
+		peer_id = generateClientID();
+
+		// Set the amount of bytes left to download (report this to the tracker)
 		amount_left = Integer.toString(torrent.file_length);
 
 		// HashMap to store GET parameters as key-value pairs
@@ -85,9 +95,9 @@ public class RUBTClient {
 		// Parameters values
 		String[] values = new String[]{
 				hash_hex,                            // SHA1 Hash (in HEX and URL encoded)
-				PEER_ID,                            // Peer ID of this client
-				// TO DO: Change port to use after a port verification has been done
-				LOWEST_PORT_TO_USE,                    // Port this client is/will using/use
+				peer_id,                            // Peer ID of this client
+				// PHASE 2: Change port to use after a port verification has been done
+				LOWEST_PORT_TO_USE,                  // For now, port is irrelevant since we're only downloading
 				amount_uploaded,
 				amount_downloaded,
 				amount_left,
@@ -101,21 +111,26 @@ public class RUBTClient {
 		// Create new TrackerCommunicator
 		TrackerCommunicator tracker = new TrackerCommunicator(torrent.announce_url.toString(), parameters);
 
-		printlnLog("Testing 1 - Send Http GET request");
-		tracker.get();
+		printlnLog("Starting download - Notify tracker and retrieve peers dictionary");
+		Map<ByteBuffer,Object> peers_dictionary = (Map<ByteBuffer,Object>) Bencoder2.decode(tracker.announceStarted(amount_left));
+
+		List<Peer> peers = tracker.getListOfPeers(peers_dictionary);
 
 		// temporary use first peer from the list of chosen peers
-		Peer peer = tracker.getPeers().get(0);
+		Peer peer = peers.get(0);
 		printlnLog("First peer : " + peer);
 
-		TorrentStats torrentStats = new TorrentStats(torrent, PEER_ID, outFile);
+		TorrentStats torrentStats = new TorrentStats(torrent, peer_id, outFile);
 		PeerCommunicator pc = new PeerCommunicator(peer, torrentStats);
 		pc.getFileFromPeer(peer);
 
+		// Notify tracker that we have completed the download
+		// Once at this point, the file download has completed without issues, so we can notify 0 bytes left
+		tracker.announceCompleted();
 	}
 
 	/* create output file if possible */
-	private static File getFile(String toFile) {
+	public static File getFile(String toFile) {
 		try {
 			Path outPath = Paths.get(toFile);
 			boolean exists = Files.exists(outPath);
@@ -141,5 +156,18 @@ public class RUBTClient {
 						+ "\nFile length: " + torrent.file_length
 						+ "\npiece_hashes array length " + torrent.piece_hashes.length
 						+ "\nSHA-1 hash value (in hex): " + hash_hex);
+	}
+
+	// Generate a random 20-byte string to use as ID
+	// The result is an alphabetic string for simplicity
+	public static String generateClientID() {
+		String id = "";
+		int id_length = 20;
+		Random randomizer = new Random();
+
+		for (int i = 0; i < 20; i++)
+			id += (char) (randomizer.nextInt(25) + 97);      // 97 is 'a' is ASCII
+
+		return id;
 	}
 }
--- src/TrackerCommunicator.java
+++ src/TrackerCommunicator.java
@@ -16,24 +16,6 @@ import static utils.Utils.printlnLog;
 
 public class TrackerCommunicator {
 
-	public static void main(String[] args) throws Exception, MalformedURLException {
-
-		HashMap<String, String> parameters = new HashMap<String, String>();
-
-		// Add parameters (hardcoded for now)
-		String[] values = {"%56%D3%D5%C5%4F%A6%38%A5%3F%28%0F%C9%41%10%60%EF%26%2D%FE%B1", "0123456789abcdefghij", "6881","0", "0", "100", "started"};
-
-		for (int i = 0; i < values.length; i++)
-			parameters.put(TrackerCommunicator.PARAMETER_KEYS[i], values[i]);
-
-		TrackerCommunicator tracker =
-				new TrackerCommunicator("http://128.6.171.130:6969/announce", parameters);
-
-		printlnLog("Testing 1 - Send Http GET request");
-		tracker.get();
-
-	}
-
 	public final static ByteBuffer INTERVAL_KEY = ByteBuffer.wrap(new byte[]
 			{ 'i', 'n', 't', 'e', 'r', 'v', 'a', 'l' });
 
@@ -51,7 +33,7 @@ public class TrackerCommunicator {
 
 	public static final String[] PARAMETER_KEYS = {"info_hash", "peer_id", "port", "uploaded", "downloaded", "left", "event"};
 
-	private final URL url;
+	private String baseURL;
 
 	private Map<String, String> parameters = null;
 
@@ -59,38 +41,39 @@ public class TrackerCommunicator {
 	private int interval;
 
 	public TrackerCommunicator(String baseURL, String parameters) throws MalformedURLException {
-		this.url = new URL(baseURL + "?" +  parameters);
+		this.baseURL = baseURL + "?" +  parameters;
 	}
 
 	public TrackerCommunicator(String baseURL, Map<String, String> parameters)
 			throws MalformedURLException, TrackerCommunicatorException, UnsupportedEncodingException {
 
 		this.parameters = parameters;
-
-		// Build the url
-		String buffer = baseURL + "?";
-
-		// Get only the parameters used in an HTTP GET request for the tracker
-		// Any other key-value pairs passed will be ignored
-		for (String key : PARAMETER_KEYS) {
-			// If one of these returns null, throw exception (except for event)
-			if (parameters.get(key) == null)
-				throw new TrackerCommunicatorException("One of the required GET parameters is NULL");
-
-			buffer += key + "=" + parameters.get(key) + "&"; //TODO: encode URL properly
-		}
-
-		// Remove the trailing '&'
-		buffer = buffer.replaceAll("&?$", "");
-
-		this.url = new URL(buffer);
+		this.baseURL = baseURL;
 	}
 
 	/* HTTP GET request to a tracker. Returns response dictionary with information about peers
 	Dictionary consist of the following keys:
 	'failure reason' optional, 'interval', 'complete' optional, 'incomplete' optional, 'peers' (: peer id, ip, port)
 	*/
-	public Map<ByteBuffer,Object> get() throws Exception {
+	public byte[] get() throws Exception {
+
+		// Build the url
+		String buffer = this.baseURL + "?";
+
+		// Get only the parameters used in an HTTP GET request for the tracker
+		// Any other key-value pairs passed will be ignored
+		for (String key : PARAMETER_KEYS) {
+			// If one of these returns null, throw exception (except for event)
+			if (this.parameters.get(key) == null)
+				throw new TrackerCommunicatorException("One of the required GET parameters is NULL");
+
+			buffer += key + "=" + this.parameters.get(key) + "&";
+		}
+
+		// Remove the trailing '&'
+		buffer = buffer.replaceAll("&?$", "");
+
+		URL url = new URL(buffer);
 
 		HttpURLConnection connection = (HttpURLConnection) url.openConnection();
 
@@ -104,35 +87,48 @@ public class TrackerCommunicator {
 		// Read and keep response in bytes
 		byte[] response = getByteArray(connection);
 
-		Map<ByteBuffer,Object> response_dictionary = (Map<ByteBuffer,Object>) Bencoder2.decode(response);
-
-		// Get the list of peers (dictionaries)
-		List<Map> peers_list = (List<Map>) response_dictionary.get(PEERS_KEY);
-
-		interval = Integer.parseInt(response_dictionary.get(INTERVAL_KEY).toString());
-		System.out.println("Intervals: " + interval);
-
-		// Decode dictionaries and print info
-		peers = new ArrayList<Peer>();
-		Iterator<Map> peers_iterator = peers_list.iterator();
-		for (int i = 0; peers_iterator.hasNext(); i++) {
-
-			Map<ByteBuffer, Object> peer_dictionary = peers_iterator.next();
-
-			System.out.println(peer_dictionary);
-
-			String peer_id = new String(((ByteBuffer) peer_dictionary.get(PEER_ID_KEY)).array(), "ASCII");
-			String peer_ip = new String(((ByteBuffer) peer_dictionary.get(IP_KEY)).array(), "ASCII");
-			Integer peer_port = (Integer)peer_dictionary.get(PORT_KEY);
-
-			addPeer(peer_id, peer_ip, peer_port);
-
-			System.out.println("Peer #" + i + ": ID - " + peer_id
-					+ "  ,  IP - " + peer_ip + "  ,  PORT - " + peer_dictionary.get(PORT_KEY));
-		}
-
-		return response_dictionary;
+		return response;
 	}
+
+    // Get list of peers from a dictionary response from the tracker
+    public List<Peer> getListOfPeers(Map<ByteBuffer,Object> dictionary) throws Exception {
+        // Get the list of peers (dictionaries)
+        List<Map> peers_list = (List<Map>) dictionary.get(PEERS_KEY);
+
+        interval = Integer.parseInt(dictionary.get(INTERVAL_KEY).toString());
+        System.out.println("Intervals: " + interval);
+
+        // Decode dictionaries and print info
+        peers = new ArrayList<Peer>();
+        Iterator<Map> peers_iterator = peers_list.iterator();
+        for (int i = 0; peers_iterator.hasNext(); i++) {
+
+            Map<ByteBuffer, Object> peer_dictionary = peers_iterator.next();
+
+            String peer_id = new String(((ByteBuffer) peer_dictionary.get(PEER_ID_KEY)).array(), "ASCII");
+            String peer_ip = new String(((ByteBuffer) peer_dictionary.get(IP_KEY)).array(), "ASCII");
+            Integer peer_port = (Integer)peer_dictionary.get(PORT_KEY);
+
+            addPeer(peer_id, peer_ip, peer_port);
+        }
+
+        return peers;
+    }
+
+    // Communicate to tracker that this download has just started
+    // Left is the amount of bytes left to download
+    public byte[] announceStarted(String left) throws Exception {
+   		setParameter("event", "started");
+		setParameter("left", left);
+		return get();
+    }
+
+    // Communicate to tracker that download is complete
+    public void announceCompleted() throws Exception {
+    	setParameter("event", "completed");
+		setParameter("left", "0");
+		get();
+    }
 
 	/* add only specific peers to the list of peers */
 	/* for Phase1 we must use peer with -RU prefix*/
@@ -166,6 +162,10 @@ public class TrackerCommunicator {
 
 	public List<Peer> getPeers() {
 		return peers;
+	}
+
+	public void setParameter(String key, String value) {
+		parameters.put(key, value);
 	}
 }
 
