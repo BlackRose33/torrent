93f88bf78e749b35074004899fdc7a9b4c16b888
Roberto Arias <walom.090@gmail.com>
2015-10-31 15:24:16 -0400

    Replaced MsgType interface for Enum

--- src/model/MsgType.java
+++ src/model/MsgType.java
@@ -1,16 +1,34 @@
 package model;
 
-/** group 16
- * Created by nadiachepurko on 10/3/15.
- */
-public interface MsgType {
-    byte CHOKE = 0;
-    byte UNCHOKE = 1;
-    byte INTERESTED = 2;
-    byte NOT_INTERESTED = 3;
-    byte HAVE = 4;
-    byte BITFIELD = 5;
-    byte REQUEST = 6;
-    byte PIECE = 7;
-    byte CANCEL = 8;
+
+public enum MsgType {
+
+	// MessageType (MessageID - first bit, Payload length - bytes)
+	// If Payload length is -x, means it's of variable length (x + ?)
+	CHOKE         (0, 0),
+	UNCHOKE       (1, 0),
+	INTERESTED    (2, 0),
+	UNINTERESTED  (3, 0),
+	HAVE          (4, 4),
+	BITFIELD      (5, -1),
+	REQUEST       (6, 13),
+	PIECE         (7, -9),
+	CANCEL        (8, 13);
+
+	private final byte id;
+	private final int payloadLength;
+
+	MsgType(int id, int payloadLength) {
+		this.id = (byte) id;
+		this.payloadLength = payloadLength;
+	}
+
+	private byte id()              { return id; }
+	private int  payloadLength()   { return payloadLength; }
+
+	public static void main(String[] args) {
+		MsgType msgtype = MsgType.PIECE;
+
+		System.out.println(msgtype.id() + " - " + msgtype.payloadLength());
+	}
 }
